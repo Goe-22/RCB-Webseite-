16fb52af334697c9df2ba48dc55005fd99aafa75
Goe-22 <christian.goehler@freenet.de>
2026-07-12 09:50:46 +0200

    Create app.js
    JavaScript Grundstruktur erstellt

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+/*
+=======================================================
+RCB Webseite
+Version 0.1
+JavaScript
+=======================================================
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("RCB Webseite erfolgreich gestartet.");
+
+});
