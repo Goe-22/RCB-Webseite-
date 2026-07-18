eaf60a46e8418b37b610b91600e15581b95b6d91
Goe-22 <christian.goehler@freenet.de>
2026-07-18 22:36:26 +0200

    app.js aktualisieren

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,49 @@
 /*
-=======================================================
-RCB Webseite
-Version 0.1
-JavaScript
-=======================================================
+==========================================================
+RC Bande Zwickau e.V.
+Version 1.0
+app.js
+==========================================================
 */
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("RCB Webseite erfolgreich gestartet.");
+    /* ==========================================
+       Elemente
+    ========================================== */
+
+    const menuButton = document.querySelector(".menu-toggle");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const overlay = document.querySelector(".mobile-overlay");
+
+    /* ==========================================
+       Menü öffnen
+    ========================================== */
+
+    if (menuButton && mobileMenu && overlay) {
+
+        menuButton.addEventListener("click", () => {
+
+            mobileMenu.classList.add("active");
+            overlay.classList.add("active");
+
+        });
+
+    }
+
+    /* ==========================================
+       Menü schließen
+    ========================================== */
+
+    if (overlay) {
+
+        overlay.addEventListener("click", () => {
+
+            mobileMenu.classList.remove("active");
+            overlay.classList.remove("active");
+
+        });
+
+    }
 
 });
