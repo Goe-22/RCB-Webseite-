0c391d4476da323a31a75fbd3819761cd092fb65
Goe-22 <christian.goehler@freenet.de>
2026-07-24 17:44:56 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+define('APP_NAME', 'RC Bande Zwickau');
+
+define('APP_VERSION', '1.0');
+
+define('APP_URL', 'http://localhost/RCB-Webseite');
+
+define('TIMEZONE', 'Europe/Berlin');
+
+date_default_timezone_set(TIMEZONE);
+
+define('UPLOAD_PATH', '../uploads/');
+
+define('BACKUP_PATH', '../backups/');
+
+define('LOG_PATH', '../logs/');
